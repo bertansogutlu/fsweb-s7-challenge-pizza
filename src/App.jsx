1b6345cd5e97352fb1d8be7ff46eb55b176760c3
Bertan Sogutlu <bertansogutlu@gmail.com>
2024-03-11 16:54:10 +0300

    orderPizza head eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './reset.css'
 import './App.css'
 import Header from './components/Header'
+import OrderPizza from './components/OrderPizza'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
+      <OrderPizza/>
     </>
   )
 }
---
+++ src/components/OrderPizza.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function OrderPizza() {
+  return (
+    <div className='orderPizza'>
+        <div className='orderPizza-head'>
+            <div className='orderPizza-head-container'>
+                <img src="../Assets/mile1-assets/logo.svg" alt="" />
+                <nav>
+                <a href="/html/">Anasayfa</a>-
+                <a href="/css/">Secenekler</a>-
+                <a href="/js/">Siparis Olustur</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default OrderPizza
