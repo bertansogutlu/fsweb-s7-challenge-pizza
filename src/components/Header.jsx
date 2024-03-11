821d65fea9d43278464ee8c00542cca35a6c7d28
Bertan Sogutlu <bertansogutlu@gmail.com>
2024-03-11 16:31:23 +0300

    header button eklendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ function Header() {
      <div className='header-container'>
         <img src="../Assets/mile1-assets/logo.svg" alt="" />
         <h2>KOD ACIKTIRIR< br />PIZZA DOYURUR</h2>
+        <button>ACIKTIM</button>
       </div>
     </div>
   )
