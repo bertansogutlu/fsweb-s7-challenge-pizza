a2892343c6a206b41e05848d20dc7c9f45526875
Bertan Sogutlu <bertansogutlu@gmail.com>
2024-03-12 20:10:46 +0300

    data.json dosyasi eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './reset.css'
 import './App.css'
 import Header from './components/Header'
 import OrderPizza from './components/OrderPizza'
+import {pizza,data,sayfa} from './data.json'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <OrderPizza/>
+      <OrderPizza pizza={pizza} data={data} sayfa={sayfa}/>
     </>
   )
 }
--- src/components/OrderPizza.jsx
+++ src/components/OrderPizza.jsx
@@ -3,25 +3,7 @@ import Form from '../helpers/Form'
 import Pizza from '../helpers/Pizza'
 import Nav from '../helpers/Nav'
 
-const pizza = {
-    ad: "Pizza Funghi",
-    fiyat: 85.50,
-    puan: 4,
-    yorumSayisi: 200,
-    aciklama: "Doğanın lezzetini sofranıza getirin! Yoğun mantar aroması ve mozarella peyniri ile enfes bir buluşma. Lezzetin doruklarına çıkın, her ısırıkta ormanın ferahlığını hissedin!"
-}
-
-const data = {
-    boyutlar: ["Kucuk", "Orta", "Buyuk"],
-    hamurlar: ["Ince", "Orta", "Kalin"],
-    malzemeler: ["Sosis","Salam","Sucuk"]
-}
-
-const sayfa = {
-    yonlendirmeler: ["Ana Sayfa", "Secenekler", "Siparis Olustur"]
-}
-
-function OrderPizza() {
+function OrderPizza({pizza,data,sayfa}) {
 
   return (
     <main className='orderPizza'>
