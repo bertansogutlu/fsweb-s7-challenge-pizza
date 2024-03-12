ad5a3af39b4eaca0281faedd954647f30371f2de
Bertan Sogutlu <bertansogutlu@gmail.com>
2024-03-12 15:41:47 +0300

    header duzenlendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='header'>
+    <section className='header'>
      <div className='header-container'>
         <img src="../Assets/mile1-assets/logo.svg" alt="logo" />
         <h1>KOD ACIKTIRIR< br />PIZZA DOYURUR</h1>
         <button>ACIKTIM</button>
       </div>
-    </div>
+    </section>
   )
 }
 
