708f0f7b38c99af363d5d5596223a125a3cdd5eb
Bertan Sogutlu <bertansogutlu@gmail.com>
2024-03-12 19:37:53 +0300

    v2

--- src/components/OrderPizza.jsx
+++ src/components/OrderPizza.jsx
@@ -15,77 +15,75 @@ const data = {
     malzemeler: ["Sosis","Salam","Sucuk"]
 }
 
+const yonlendirmeKarti = (yonlendirmeler) => { 
+    return (
+        <div className='yonlendirmeKarti'>
+            <nav>
+                {yonlendirmeler.map((yonlendirme,index) => <a href={`/${yonlendirme}/`} key={index}>{yonlendirme}</a>)}
+            </nav>
+        </div>
+)}
+
+const pizzaKarti = (pizza) => { 
+    return (
+        <div className='pizzaKarti'>
+            <h2>{pizza.ad}</h2>
+            <div>
+                <h3>{pizza.fiyat}₺</h3><p>{pizza.puan}</p><p>({pizza.yorumSayisi})</p>
+            </div>
+            <p>{pizza.aciklama}</p>
+        </div>
+)}
+
+const boyutKarti = (boyutlar) => { 
+    return (
+        <div className='boyutKarti'>
+            <h2>Boyut Sec</h2>
+            <div>
+                {boyutlar.map((boyut,index) => <label key={index}><input type="radio" />{boyut}</label>)}
+            </div>
+        </div>
+)}
+
+const hamurKarti = (hamurlar) => { 
+    return (
+        <div className='hamurKarti'>
+            <label>
+                <h2>Hamur Sec</h2>
+                <select name="hamur" id="hamur">
+                {hamurlar.map((hamur,index) => <option value={hamur} key={index}>{hamur}</option>)}
+                </select>
+            </label>
+        </div>
+)}
+
+const malzemeKarti = (malzemeler) => { 
+    return (
+        <div className='malzemeKarti'>
+            <h2>EK Malzemeler</h2>
+            <p>En fazla 10 malzeme secebilirsiniz</p>
+            <div>
+                {malzemeler.map((malzeme,index)=>
+                <label key={index}>
+                    <span>{malzeme}</span><input type="checkbox" />
+                    
+                </label>
+                )}
+            </div>
+        </div>
+)}
+
+const notKarti = () => { 
+    return (
+        <div className='notKarti'>
+            <label>
+            <h2>Siparis Notu</h2>
+            <textarea name="not" id="not"></textarea>
+            </label>
+        </div>
+)}
+
 function OrderPizza() {
-
-    const yonlendirmeKarti = (yonlendirmeler) => { 
-        return (
-            <div className='yonlendirmeKarti'>
-                <nav>
-                    {yonlendirmeler.map((yonlendirme,index) => <a href={`/${yonlendirme}/`} key={index}>{yonlendirme}</a>)}
-                </nav>
-            </div>
-    )}
-
-    const pizzaKarti = (pizza) => { 
-        return (
-            <div className='pizzaKarti'>
-                <h2>{pizza.ad}</h2>
-                <div>
-                    <h3>{pizza.fiyat}₺</h3><p>{pizza.puan}</p><p>({pizza.yorumSayisi})</p>
-                </div>
-                <p>{pizza.aciklama}</p>
-            </div>
-    )}
-
-    const boyutKarti = (boyutlar) => { 
-        return (
-            <div className='boyutKarti'>
-                <h2>Boyut Sec</h2>
-                <div>
-                    {boyutlar.map((boyut,index) => <label key={index}><input type="radio" />{boyut}</label>)}
-                </div>
-            </div>
-    )}
-
-    const hamurKarti = (hamurlar) => { 
-        return (
-            <div className='hamurKarti'>
-                <label>
-                    <h2>Hamur Sec</h2>
-                    <select name="hamur" id="hamur">
-                    {hamurlar.map((hamur,index) => <option value={hamur} key={index}>{hamur}</option>)}
-                    </select>
-                </label>
-            </div>
-    )}
-
-    const malzemeKarti = (malzemeler) => { 
-        return (
-            <div className='malzemeKarti'>
-                <h2>EK Malzemeler</h2>
-                <p>En fazla 10 malzeme secebilirsiniz</p>
-                <div>
-                    {malzemeler.map((malzeme,index)=>
-                    <label key={index}>
-                        <input type="checkbox" />
-                        <h3>{malzeme}</h3>
-                    </label>
-                    )}
-                </div>
-            </div>
-    )}
-
-    const notKarti = () => { 
-        return (
-            <div className='notKarti'>
-                <label>
-                <h2>Siparis Notu</h2>
-                <textarea name="not" id="not"></textarea>
-                </label>
-            </div>
-    )}
-
-
 
   return (
     <main className='orderPizza'>
@@ -95,8 +93,8 @@ function OrderPizza() {
                 {yonlendirmeKarti(data.yonlendirmeler)}
             </div>
         </section>
-        <section>
-            <div className='orderPizza-main'>
+        <section className='orderPizza-main'>
+            <div className='orderPizza-main-container'>
                 {pizzaKarti(pizza)}
                 <form action="">
                     <div>
